c9e4d79ea591179bc8d5697677b7b5e919031b5b
Sarah Carter <carters@transblue.org>
2023-12-11 16:57:51 -0800

    removed react-reveal package

--- src/components/contactCta/index.js
+++ src/components/contactCta/index.js
@@ -1,32 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import Form from './form';
-import Slide from 'react-reveal/Slide';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import Form from "./form";
+// import Slide from 'react-reveal/Slide';
+import "./style.css";
 
 function ContactCta(props) {
-    const [displayForm, setDisplayForm] = useState(false);
-    const [displayCTA, setDisplayCTA] = useState(false);
+  const [displayForm, setDisplayForm] = useState(false);
+  const [displayCTA, setDisplayCTA] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if(window.scrollY > 400) setDisplayCTA(true)
-            else setDisplayCTA(false)
-        })
-    })
-    return(
-        displayCTA && <div className='contact-cta'>
-            <button id='cta' onClick={() => setDisplayForm(true)}>CONTACT US</button>
-            <Slide right when={displayForm} exit={true} collapse>
-                <div className='form'>
-                    <Form 
-                        email={props.email} 
-                        location={props.location} 
-                        setDisplayForm={setDisplayForm} 
-                    />
-                </div>
-            </Slide>
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) setDisplayCTA(true);
+      else setDisplayCTA(false);
+    });
+  });
+  return (
+    displayCTA && (
+      <div className="contact-cta">
+        <button id="cta" onClick={() => setDisplayForm(true)}>
+          CONTACT US
+        </button>
+        {/* <Slide right when={displayForm} exit={true} collapse> */}
+        <div className="form">
+          <Form
+            email={props.email}
+            location={props.location}
+            setDisplayForm={setDisplayForm}
+          />
         </div>
+        {/* </Slide> */}
+      </div>
     )
+  );
 }
 
 export default ContactCta;
