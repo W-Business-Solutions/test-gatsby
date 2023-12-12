f2d5463e25e163d3349850d6ccd2f576ce98d404
Sarah Carter <carters@transblue.org>
2023-12-12 11:21:40 -0800

    replaced themecontext with screensizecontext

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ThemeContext from "./src/contextWrappers/themeContext";
-// import { ScreenSizeContextWrapper } from "./src/contextWrappers/screenSizeContext";
+import ScreenSizeContextWrapper from "./src/contextWrappers/screenSizeContext";
 import("./src/styles/global.css");
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeContext>
-    {/* <ScreenSizeContextWrapper> */}
-    {element}
-    {/* </ScreenSizeContextWrapper> */}
-  </ThemeContext>
+  <ScreenSizeContextWrapper>{element}</ScreenSizeContextWrapper>
 );
