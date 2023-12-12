fa443e48abf7b697591fbf6a10eb57f62a76b5ce
Sarah Carter <carters@transblue.org>
2023-12-12 10:44:07 -0800

    updated intertheme and font theme in themecontext

--- src/contextWrappers/themeContext.js
+++ src/contextWrappers/themeContext.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { createTheme } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
 
 function ThemeContext({ children }) {
   const fontTheme = createTheme();
-  const interTheme = createTheme();
+  fontTheme = responsiveFontSizes(fontTheme);
+  const interTheme = createTheme({
+    root: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+    },
+    typography: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      a: {
+        textDecoration: "none",
+      },
+    },
+    MuiButton: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          fontFamily: ["Inter", "sans-serif"].join(","),
+        },
+      },
+    },
+    MuiLink: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          fontFamily: ["Inter", "sans-serif"].join(","),
+        },
+      },
+    },
+  });
 
   return (
     <ThemeProvider theme={fontTheme}>
