f7e3730ac55600ed96dcf756b92896a91f5cd593
Sarah Carter <carters@transblue.org>
2023-12-12 11:18:15 -0800

    removed screensizecontext

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ThemeContext from "./src/contextWrappers/themeContext";
-import { ScreenSizeContextWrapper } from "./src/contextWrappers/screenSizeContext";
+// import { ScreenSizeContextWrapper } from "./src/contextWrappers/screenSizeContext";
 import("./src/styles/global.css");
 
 export const wrapRootElement = ({ element }) => (
   <ThemeContext>
-    <ScreenSizeContextWrapper>{element}</ScreenSizeContextWrapper>
+    {/* <ScreenSizeContextWrapper> */}
+    {element}
+    {/* </ScreenSizeContextWrapper> */}
   </ThemeContext>
 );
